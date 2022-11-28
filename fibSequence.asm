47d0bacaaed3f644527c772fce2fb5ead0c52325
aaa <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2022-11-28 16:48:24 -0500

    dgdfgfad

--- fibSequence.asm
+++ fibSequence.asm
@@ -7,77 +7,45 @@
 ; Since: 2022-11-28                             ;
 ; ----------------------------------------------;
 
-; System Call Definitions
-;-----------------------------------------------;
-SYS_WRITE equ 1 ; write to _
-SYS_EXIT equ 60 ; end program
-STDOUT equ 1    ; define standard output
-;-----------------------------------------------;
-
 section .bss
-  numLen equ 2                                    ; length of output
-  num resb numLen                                ; variable to hold numbers
+  someNumber: RESD 1                     ; 4 bytes
 
 section .data
-  newline: db 10                                   ; represents new line
-  newlineLen: equ $-newline                        ; length of new line
-  opening: db "Printing Fibonacci Sequence...", 10 ; first line to be printed
-  openingLen: equ $-opening                        ; length of opneing
-  done: db "Done.", 10                             ; ending line
-  doneLen: equ $-done                              ; length of done
+
+  ; constants here
+  newLine: db 10                       ; UNICODE 10 is new line character
+  done: db 10, "Done.", 10             ; string to print
+  doneLen: equ $-done                  ; length of string
 
 section .text
-  global_start:                                    ; entry point for linker
+  global _start                        ; entry point for linker
 
-  _start:
-    ; print opening
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, opening
-    mov rdx, openingLen
-    syscall
-
-    ; print new line
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, newline
-    mov rdx, newlineLen
-    syscall
-
-    ; move 0 into r8, 9 into r9
-    mov r8, -1
-    mov r9, 9 -1
+  _start:                              ; start here
+    mov r8, -1                       ; move the integer 0 into r8
+    mov r9, 9 -1                     ; move the integer 9 into r9
 
     IncrementLabel:
-      ; do while loop
+      ; doing a do ... while loop!
       inc r8
-      inc r9
-      call PrintSingleDigitInt     ; call single digit function
+      push r8
+      call PrintSingleDigitInt     ; call our print single digit function
       add rsp, 4                   ; pop but throw away the value
       cmp r8, 9-1                  ; compare r8 and ascii 9
       jle IncrementLabel           ; jump if <= goto "LoopLable"
 
-    ; print new line
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, newline
-    mov rdx, newlineLen
-    syscall
+      ; write done to screen
+      mov rax, 1                  ; system call for write
+      mov rdi, 1                  ; file handle 1 is stdout
+      mov rsi, done               ; address of string to output
+      mov rdx, doneLen            ; number of bytes
+      syscall                     ; invoke operating system to do the write
 
-    ; print done
-    mov rax, SYS_WRITE         ; sys_write
-    mov rdi, STDOUT            ; stdout
-    mov rsi,done               ; line to write
-    mov rdx,doneLen            ; line length
-    syscall                    ; call kernal
-
-    ; end program with error code 0 (success)
-    mov rax, SYS_EXIT
-    mov rdi, 0
-    syscall
+      mov rax, 60                 ; system call for exit
+      mov rdi, 0                  ; exit code 0
+      syscall                     ; invoke operating system to exit
 
 PrintSingleDigitInt:
-  ; takes in a single digit int and prints the ascii equivalent
+  ; takes in a single digit int and prints the assci equivalent
 
   ; when a function is called, the return value is placed on the stack
   ; we need to keep this, so that we can return to the corret place in our program!
@@ -87,18 +55,20 @@ PrintSingleDigitInt:
   push r15                    ; place it back onto the stack
 
   ; write value on the stack to STDOUT
-  mov rax, SYS_WRITE          ; system call for write
-  mov rdi, STDOUT             ; file handle 1 is stdout
+  mov rax, 1                  ; system call for write
+  mov rdi, 1                  ; file handle 1 is stdout
   mov rsi, rsp                ; the string to write popped from the top of the stack
   mov rdx, 1                  ; number of bytes
   syscall                     ; invoke operating system to do the write
+  ;add     $8, %rsp    # restore sp -> not sure this is neede!
+  ; https://stackoverflow.com/questions/8201613/printing-a-character-to-standard-output-in-assembly-x86
 
-  ; print new line
-  mov rax, SYS_WRITE
-  mov rdi, STDOUT
-  mov rsi, newline
-  mov rdx, newlineLen
-  syscall
+  ; print a new line
+  mov rax, 1                  ; system call for write
+  mov rdi, 1                  ; file handle 1 is stdout
+  mov rsi, newLine            ; address of string to output
+  mov rdx, 1                  ; number of bytes
+  syscall                     ; invoke operating system to do the write
 
   push r14                    ; put the return address back on the stack to get back
   ret                         ; return
